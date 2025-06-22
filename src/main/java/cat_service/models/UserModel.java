c917aec61abbff3f7295a43f482b4c914124110e
Aleksey <a.e.sokolkov@gmail.com>
2025-06-22 15:52:16 +0500

    feat: service-mvp

---
+++ src/main/java/cat_service/models/UserModel.java
@@ -0,0 +1,10 @@
+package cat_service.models;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+
+  private Long id;
+  private String username;
+}
---
+++ src/main/java/cat_service/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package cat_service.repositories;
+
+import cat_service.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {}
