032e21c480bc3f14ed6cfc9a34ababec132d20e9
Aleksey <a.e.sokolkov@gmail.com>
2025-09-14 22:10:35 +0500

    fix: bot starting

--- src/main/java/cat_service/telegram/TelegramFacade.java
+++ src/main/java/cat_service/telegram/TelegramFacade.java
@@ -1,9 +1,7 @@
 package cat_service.telegram;
 
-import cat_service.configs.TelegramConfigs;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
@@ -13,20 +11,10 @@ public class TelegramFacade implements LongPollingSingleThreadUpdateConsumer {
 
   private final TelegramMessageHandler handler;
   private final TelegramMessageConverter converter;
-  private final TelegramConfigs configs;
 
   @Override
   public void consume(Update update) {
     var dto = converter.convert(update);
     handler.handle(dto);
-  }
-
-  public void Start() {
-    try (var bot = new TelegramBotsLongPollingApplication()) {
-      bot.registerBot(configs.getToken(), this);
-      Thread.currentThread().join();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
   }
 }
